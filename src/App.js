34e256bdf3471a24c67ebdf4e0b84ec04175aa0d
Karla Elizabeth <elizabeth.lala99@gmail.com>
2022-06-28 18:11:55 -0500

    fix:what is pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [allPokemons, setAllPokemons] = useState([])
+  const [laodMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+  
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data =await res.json()
+
+    setLoadMore(data.next)
+   
+    function craetePokemonObject (result) {
+result.forEach( async (pokemon) => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/${pokemon.name}")
+  const data =await  res.json()
+
+ setAllPokemons(currentlist => [...currentlist,data])
+
+ await console.log (allPokemons)
+})
+    }
+    craetePokemonObject(data.result)
+  }
+  
+  useEffect(() => {
+    getAllPokemons()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App-container">
+     <h1>pokemon evolution</h1>
+     <div className="pokemon-container">
+      <div className="all-container">
+
+      </div>
+      <button className="load-more">Load more</button>
+     </div>
     </div>
   );
 }
