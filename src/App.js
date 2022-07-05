12098f895052fae1d9e6233a17b1ccdfe6a9f524
Karla Elizabeth <elizabeth.lala99@gmail.com>
2022-07-05 01:56:37 -0500

    fix: pokemonapp

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import PokemonThumb from './components/PokemonThumb'
+import PokemonDetails from './components/PokemonDetails'
 
-function App() {
+const App = () => {
 
-  const [allPokemons, setAllPokemons] = useState([])
-  const [laodMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
-  
+   const[allPokemons, setAllPokemons] = useState([])
+   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
   const getAllPokemons = async () => {
-    const res = await fetch(laodMore)
-    const data =await res.json()
+    const res = await fetch(loadMore)
+    const data = await res.json()
 
     setLoadMore(data.next)
-   
-    function craetePokemonObject (result) {
-      result.forEach( async (pokemon) => {
-       const res = await fetch("https://pokeapi.co/api/v2/pokemon/${pokemon.name}")
-       const data =await  res.json()
-
-      setAllPokemons(currentlist => [...currentlist,data])
-
-    })
+// eslint-disable-next-line
+    function createPokemonObject(results)  {
+      results.forEach( async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data =  await res.json()
+        setAllPokemons( currentList => [...currentList, data])
+        await allPokemons.sort((a, b) => a.id - b.id)
+      })
     }
-    craetePokemonObject(data.result)
-    await console.log(allPokemons)
+    createPokemonObject(data.results)
   }
-  
-  useEffect(() => {
-    getAllPokemons()
-  },[])
-  return (
 
-    <div className="App-container">
-     <h1>pokemon evolution</h1>
-     <div className="pokemon-container">
-      <div className="all-container">
-          { allPokemons.map(pokemon => <li>{pokemon.name}</li>)}
+ useEffect(() => {
+  getAllPokemons()
+ }, [])
+// eslint-disable-next-line
+  return (
+    <div className="app-contaner">
+      <h1>Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemons.map( (pokemonStats, index) => 
+            <PokemonThumb
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />)}
+          
+        </div>
+          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
       </div>
-      <button className="load-more">Load more</button>
-     </div>
     </div>
   );
 }
